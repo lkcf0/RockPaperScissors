7f27a3a4039827a83fc07d540b68c4a4c27c7642
lkcf0 <lkcf@tutanota.com>
2022-05-17 22:18:29 -0500

    added functions for playing a round used if else statement for player win conditions

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,4 +3,31 @@ function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
-console.log(computerPlay());
+
+// function to play a round//
+function playRound(playerSelection) {
+// call computerPlay() and store in a variable //
+    let computerSelection = computerPlay()
+    console.log(computerSelection);
+// ask play from input and store in parameter//
+    playerSelection = prompt('Rock, Paper or Scissors?');
+    console.log(playerSelection);
+    
+ // player win conditions//   
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+       (playerSelection == 'scissors' && computerSelection == 'paper') ||
+       (playerSelection == 'paper' && computerSelection == 'rock')) {
+        alert('You win!' + playerSelection + ' beats ' + computerSelection);
+    }
+// if both computer player and player have same result
+    else if (playerSelection == computerSelection) {
+        alert('Try again, Its a tie!');
+    }
+// when computer wins // 
+    else  
+    {
+        alert('You lose!' + computerSelection + ' beats ' + playerSelection);
+    }
+}
+
+ playRound()
