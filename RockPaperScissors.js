0e2deb7175bded09f302e2e1cb20d55f24a54309
lkcf0 <lkcf@tutanota.com>
2022-05-18 17:17:58 -0500

    made user input case-insensitive

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -6,28 +6,30 @@ function computerPlay() {
 
 // function to play a round//
 function playRound(playerSelection) {
+
 // call computerPlay() and store in a variable //
     let computerSelection = computerPlay()
     console.log(computerSelection);
+
 // ask play from input and store in parameter//
-    playerSelection = prompt('Rock, Paper or Scissors?');
-    console.log(playerSelection);
-    
- // player win conditions//   
+
+    playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
        (playerSelection == 'scissors' && computerSelection == 'paper') ||
        (playerSelection == 'paper' && computerSelection == 'rock')) {
-        alert('You win!' + playerSelection + ' beats ' + computerSelection);
+        alert('You win! ' + playerSelection + ' beats ' + computerSelection);
     }
 // if both computer player and player have same result
     else if (playerSelection == computerSelection) {
         alert('Try again, Its a tie!');
     }
+
 // when computer wins // 
     else  
     {
-        alert('You lose!' + computerSelection + ' beats ' + playerSelection);
+        alert('You lose! ' + computerSelection + ' beats ' + playerSelection);
     }
+
 }
 
  playRound()
